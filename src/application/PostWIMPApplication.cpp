ee418e7850914b6b8a80cb6d7baff2d6cf3a76b3
Gauthier Zirnhelt <gauthier.zirnhelt@ensimag.imag.fr>
2014-11-26 16:34:45 +0100

    réoptimisation add/remove

--- src/application/PostWIMPApplication.cpp
+++ src/application/PostWIMPApplication.cpp
@@ -70,7 +70,7 @@ void PostWIMPApplication::step()
                     ));
     }
 
-    static const uint32_t colorMap[]=
+    /*static const uint32_t colorMap[]=
     {
         0xFF0000FF,
         0x00FF00FF,
@@ -78,15 +78,15 @@ void PostWIMPApplication::step()
         0x00FFFFFF,
         0xFF00FFFF,
         0xFFFF00FF,
-    };
+    };*/
     //static uint32_t currentColor = 0xFF0000FF;
 
 
     //TODO: Input dans GameActionControllerXYZ
-    static int colorIndex = 0;
+    /*static int colorIndex = 0;
     if (Input::isKeyPressed(GLFW_KEY_SPACE))
         //currentColor = colorMap[++colorIndex%6];
-        gameAction.pencil.color = colorMap[++colorIndex%6];
+        gameAction.pencil.color = colorMap[++colorIndex%6];*/
 
     const int T = 10 * gameAction.pencil.size;
 
@@ -94,27 +94,38 @@ void PostWIMPApplication::step()
     int py = gameAction.pencil.position.y;
     int pz = gameAction.pencil.position.z;
 
-    //TODO: pointeur fonction lerp
-    for(int x = -T/2; x<=T/2; ++x)
-        for(int y = -T/2; y<=T/2; ++y)
-            for(int z = -T/2; z<=T/2; ++z)
-            {
-                float level = x*x+y*y+z*z;
-                level *= 0.3 * gameAction.pencil.strength;
-                level = exp(-level);
-                level *= 3.0;
-                if (level>1.0) level=1.0;
-                int xx = px + x;
-                int yy = py + y;
-                int zz = pz + z;
-                if (xx>=1 and xx<H and yy>=1 and yy<H and zz>1 and zz<H)
+    if (gameAction.action == GameAction::Add)
+        for(int x = -T/2; x<=T/2; ++x)
+            for(int y = -T/2; y<=T/2; ++y)
+                for(int z = -T/2; z<=T/2; ++z)
                 {
-                    if (gameAction.action == GameAction::Add)
+                    float level = x*x+y*y+z*z;
+                    level *= 0.3 * gameAction.pencil.strength;
+                    level = exp(-level);
+                    level *= 3.0;
+                    if (level>1.0) level=1.0;
+                    int xx = px + x;
+                    int yy = py + y;
+                    int zz = pz + z;
+                    if (xx>=1 and xx<H and yy>=1 and yy<H and zz>1 and zz<H)
                         voxelMap.lerp(xx,yy,zz,Voxel(gameAction.pencil.color,255),level);
-                    else if (gameAction.action == GameAction::Remove)
-                        voxelMap.lerpDensity(xx,yy,zz,Voxel(gameAction.pencil.color,0.0),level);
                 }
-            }
+    else if (gameAction.action == GameAction::Remove)
+        for(int x = -T/2; x<=T/2; ++x)
+            for(int y = -T/2; y<=T/2; ++y)
+                for(int z = -T/2; z<=T/2; ++z)
+                {
+                    float level = x*x+y*y+z*z;
+                    level *= 0.3 * gameAction.pencil.strength;
+                    level = exp(-level);
+                    level *= 3.0;
+                    if (level>1.0) level=1.0;
+                    int xx = px + x;
+                    int yy = py + y;
+                    int zz = pz + z;
+                    if (xx>=1 and xx<H and yy>=1 and yy<H and zz>1 and zz<H)        
+                        voxelMap.lerpDensity(xx,yy,zz,Voxel(gameAction.pencil.color,0),level);
+                }
 
     // update the modified region
     voxelMapDisplayer.update(PolyVox::Region(
